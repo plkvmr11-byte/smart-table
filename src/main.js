5b171e61f71751ff26d62b69004db6b95d0b8933
Mariya Polyakova <mari.polyakova1999@yandex.ru>
2026-03-23 23:02:53 +0300

    33

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ import {initSorting} from "./components/sorting.js";
 
 import {initFiltering} from "./components/filtering.js";
 
+import { initSearching } from "./components/searching.js";
+
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
 
